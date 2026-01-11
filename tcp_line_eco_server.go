eb014c9831d2fde56b81bcae3d4c7a1440edef78
Jesly Varghese <jesly.varghese@gmail.com>
2026-01-11 19:29:36 +0100

    Added wait group + go routines

--- tcp_line_eco_server.go
+++ tcp_line_eco_server.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 )
 
 func StartLineEchoServer() {
@@ -17,18 +22,44 @@ func StartLineEchoServer() {
 
 	fmt.Println("Line Echo Server listening on :9090")
 
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-sigChan
+		fmt.Println("\nShutting down Line Echo Server gracefully")
+		listener.Close()
+	}()
+
+	var wg sync.WaitGroup
+
+outer:
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Println("Error accepting connection:", err)
-			continue
+			return
 		}
 
-		go handleEchoConnection(conn)
+		select {
+		case <-ctx.Done():
+			break outer
+		default:
+			wg.Add(1)
+			go handleEchoConnection(conn, &wg)
+
+		}
 	}
+
+	wg.Wait()
+	fmt.Println("Line Echo Server shut down")
 }
 
-func handleEchoConnection(conn net.Conn) {
+func handleEchoConnection(conn net.Conn, wg *sync.WaitGroup) {
+	defer wg.Done()
 	defer conn.Close()
 
 	reader := bufio.NewReader(conn)
